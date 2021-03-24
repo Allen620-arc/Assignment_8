f3175014a9af3aafd89ef27b80902c4894bac19d
Allen Keettikkal <allenkeettikkal@gmail.com>
2021-03-24 11:37:00 -0600

    Finished and tested the to_string() function of a8q3.py

--- a8q3.py
+++ a8q3.py
@@ -5,3 +5,29 @@
     Instructor: Jeffrey Long
 """
 
+
+def to_string(node_chain):
+    """
+    Purpose:
+        Create a string representation of the node chain.  E.g.,
+        [ 1 | *-]-->[ 2 | *-]-->[ 3 | / ]
+
+    Pre-conditions:
+        :param node_chain:  A node-chain, possibly empty (None)
+
+    Post_conditions:
+        None
+
+    Return: A string representation of the nodes.
+    """
+    # If there is an empty node chain:
+    if node_chain is None:
+        return 'EMPTY'
+
+    # If there is one node in the node chain:
+    if node_chain.get_next() is None:
+        return '[ {} |'.format(str(node_chain.get_data())) + ' / ]'
+
+    # Otherwise:
+    else:
+        return '[ {} | *-]-->'.format(str(node_chain.get_data())) + to_string(node_chain.get_next())
--- a8q3_testing.py
+++ a8q3_testing.py
@@ -5,3 +5,30 @@
     Instructor: Jeffrey Long
 """
 
+import a8q3
+import node as N
+
+test_suite = [
+
+    {"inputs": None,
+     "outputs": 'EMPTY',
+     "reason": "Empty node chain."},
+
+    {"inputs": N.node(1),
+     "outputs": '[ 1 | / ]',
+     "reason": "Node chain with one node."},
+
+    {"inputs": N.node(1, N.node(2)),
+     "outputs": '[ 1 | *-]-->[ 2 | / ]',
+     "reason": "Node chain with two node."},
+
+    {"inputs": N.node(1, N.node(2, N.node(3))),
+     "outputs": '[ 1 | *-]-->[ 2 | *-]-->[ 3 | / ]',
+     "reason": "Node chain with two node."},
+]
+
+for test in test_suite:
+    inputs = test["inputs"]
+    result = a8q3.to_string(inputs)
+    if result != test["outputs"]:
+        print("Testing fault: to_string() returned", result, "on inputs", inputs, "(", test["reason"], ")")
